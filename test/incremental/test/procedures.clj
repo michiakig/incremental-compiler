0565e64e0b4b205c20ff74cbd55dcfcdfa62f789
Michiaki Yamada <spacemanaki@gmail.com>
2011-10-26 01:39:22 -0400

    added recursive sum test

--- test/incremental/test/procedures.clj
+++ test/incremental/test/procedures.clj
@@ -8,4 +8,10 @@
   (is (compiled= (letrec ((double (lambda (x) (fx+ x x)))
                           (triple (lambda (x) (fx+ x (fx+ x x)))))
                          (app double (app triple 3)))
-                 "18")))
+                 "18"))
+  (is (compiled= (letrec ((sum (lambda (x)
+                                        (if (fx= 0 x)
+                                          0
+                                          (fx+ x (app sum (fx- x 1)))))))
+                         (app sum 100))
+                 "5050")))
